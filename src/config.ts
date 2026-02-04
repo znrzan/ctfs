f8a2950108335b6f8a5f4d586edb565ee993fba0
ozanqs <165639031+znrzan@users.noreply.github.com>
2026-02-04 19:40:36 +0700

    Disable Live2D Maskot Anime component

--- src/config.ts
+++ src/config.ts
@@ -49,7 +49,7 @@ export const APP = {
   notifSolves: true, // notifikasi global saat ada yang solve challenge
   ChallengeTutorial: true, // enable / disable Challenge Tutorial component
   ChatBotAI: false, // enable / disable ChatBot AI component
-  Live2DMaskotAnime: true, // enable / disable Live2D Maskot Anime component
+  Live2DMaskotAnime: false, // enable / disable Live2D Maskot Anime component
   teams: {
     enabled: true, // enable / disable teams feature
     hideScoreboardIndividual: false, // enable / disable individual scoreboard when teams are enabled
